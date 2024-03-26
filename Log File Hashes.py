9c3e0676cfb36d218c450585b2a176abc872888c
soardev <soardev>
2024-03-26 20:10:10 +0000

    Added decision logic to comment if a hash has been previously observed as well as if it has not been previously observed. If not previously observed a comment is added as well as adding it to the "Prior Hashes" list.

--- Log File Hashes.py
+++ Log File Hashes.py
@@ -12,9 +12,86 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-
+    # call 'decision_1' block
+    decision_1(container=container)
 
     return
+
+@phantom.playbook_block()
+def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("decision_1() called")
+
+    # check for 'if' condition 1
+    found_match_1 = phantom.decision(
+        container=container,
+        conditions=[
+            ["artifact:*.cef.fileHash", "in", "custom_list:Prior Hashes"]
+        ],
+        delimiter=None)
+
+    # call connected blocks if condition 1 matched
+    if found_match_1:
+        add_comment_1(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    # check for 'elif' condition 2
+    found_match_2 = phantom.decision(
+        container=container,
+        conditions=[
+            ["artifact:*.cef.fileHash", "not in", "custom_list:Prior Hashes"]
+        ],
+        delimiter=None)
+
+    # call connected blocks if condition 2 matched
+    if found_match_2:
+        add_comment_add_to_list_2(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    return
+
+
+@phantom.playbook_block()
+def add_comment_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("add_comment_1() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.comment(container=container, comment="Hash has been previously observed.")
+
+    return
+
+
+@phantom.playbook_block()
+def add_comment_add_to_list_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("add_comment_add_to_list_2() called")
+
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.fileHash"])
+
+    container_artifact_cef_item_0 = [item[0] for item in container_artifact_data]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.comment(container=container, comment="Hash has not been previously observered.")
+    phantom.add_list(list_name="Prior Hashes", values=container_artifact_cef_item_0)
+
+    return
+
 
 @phantom.playbook_block()
 def on_finish(container, summary):
